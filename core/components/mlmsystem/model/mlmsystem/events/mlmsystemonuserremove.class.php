046f15f503270e929ad28b0eb8a4091932dbbf51
Володя Володин <vovanblya@mail.ru>
2015-10-20 12:20:03 +0400

    -fix remove user

--- core/components/mlmsystem/model/mlmsystem/events/mlmsystemonuserremove.class.php
+++ core/components/mlmsystem/model/mlmsystem/events/mlmsystemonuserremove.class.php
@@ -7,7 +7,7 @@ class MlmSystemOnUserRemove extends MlmSystemPlugin
 		$this->MlmSystem->initialize($this->modx->context->key);
 
 		$user = $this->modx->getOption('user', $this->scriptProperties, 0);
-		if ($client = $this->modx->getObject('MlmSystemClient', $user->get('id'))) {
+		if ($client = $this->modx->getObject('MlmSystemClient', array('id' => $user->get('id')))) {
 			$this->MlmSystem->Tools->changeClientStatus($client, $client->getStatusRemoved());
 		}
 	}
