7134605b8104c4c434ce83c6ce9ee2548f4ae26f
Володя Володин <vovanblya@mail.ru>
2015-10-23 13:24:06 +0400

    add - profit

---
+++ core/components/mlmsystem/model/mlmsystem/events/mlmsystemmsonchangeorderstatus.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class MlmSystemmsOnChangeOrderStatus extends MlmSystemEventPlugin
+{
+	public function run()
+	{
+		$this->MlmSystem->initialize($this->modx->context->key);
+
+
+		$this->modx->log(1, print_r('MlmSystemmsOnChangeOrderStatus', 1));
+
+	}
+
+}
