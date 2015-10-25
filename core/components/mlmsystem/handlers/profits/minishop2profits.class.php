b8e936e7c58dbb634e9f6ce47a4a3c64caee37e0
Володя Володин <vovanblya@mail.ru>
2015-10-25 12:09:18 +0400

    add MiniShop2Profits

---
+++ core/components/mlmsystem/handlers/profits/minishop2profits.class.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!class_exists('MlmSystemProfitsInterface')) {
+	require_once dirname(__FILE__) . '/systemprofits.class.php';
+}
+
+
+class MiniShop2Profits extends SystemProfits implements MlmSystemProfitsInterface
+{
+
+	/** @var modX $modx */
+	protected $modx;
+	/** @var MlmSystem $MlmSystem */
+	protected $MlmSystem;
+	/** @var MiniShop2 $MiniShop2 */
+	protected $MiniShop2;
+	/** @var array $config */
+	protected $config = array();
+
+
+	public function __construct($MlmSystem, $config)
+	{
+		$this->MlmSystem = &$MlmSystem;
+		$this->modx = &$MlmSystem->modx;
+		$this->config =& $config;
+
+		if ($this->MiniShop2 = $this->modx->getService('miniShop2')) {
+			$this->MiniShop2->initialize($this->modx->context->key);
+		} else {
+			$this->MlmSystem->printLog('MiniShop2Profits requires installed miniShop2.', 1);
+		}
+	}
+
+
+}
