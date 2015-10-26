8e9d138b3dbfb35b09d60138356a4ae0af65a386
Володя Володин <vovanblya@mail.ru>
2015-10-26 12:49:35 +0400

    add profit

--- core/components/mlmsystem/handlers/profits/minishop2profits.class.php
+++ core/components/mlmsystem/handlers/profits/minishop2profits.class.php
@@ -41,4 +41,52 @@ class MiniShop2Profits extends SystemProfits implements MlmSystemProfitsInterfac
 		return $initiator;
 	}
 
+	/** @inheritdoc} */
+	public function getProfitResourceGroups($id = 0)
+	{
+		$groups = array();
+		$key = $this->MlmSystem->namespace;
+		$options = array(
+			'cache_key' => $key . '/profit/group/' . __CLASS__ . '/resource/' . $id,
+			'cacheTime' => 0,
+		);
+		if (
+			$resource = $this->modx->getObject('msProduct', array('id' => $id)) AND
+			!$groups = $this->MlmSystem->getCache($options)
+		) {
+			$ids = $this->modx->getParentIds($id, 10, array('context' => $resource->get('context_key')));
+			$ids[] = $id;
+			$ids = array_unique($ids);
+
+			$q = $this->modx->newQuery('msCategoryMember', array('product_id' => $id));
+			$q->select('category_id');
+			$tstart = microtime(true);
+			if ($q->prepare() && $q->stmt->execute()) {
+				$this->modx->queryTime += microtime(true) - $tstart;
+				$this->modx->executedQueries++;
+				if ($tmp = $q->stmt->fetchAll(PDO::FETCH_COLUMN)) {
+					$ids = array_merge($ids, $tmp);
+				}
+			}
+			$ids = array_unique($ids);
+			
+			$q = $this->modx->newQuery('modResourceGroupResource', array('document:IN' => $ids));
+			$q->leftJoin('MlmSystemProfitGroup', 'MlmSystemProfitGroup', 'MlmSystemProfitGroup.group = modResourceGroupResource.document_group');
+			$q->where(array('MlmSystemProfitGroup.class' => 'modResourceGroup'));
+			$q->select('document_group,profit');
+			$q->sortby('profit');
+			$q->groupby('MlmSystemProfitGroup.group');
+			$tstart = microtime(true);
+			if ($q->prepare() && $q->stmt->execute()) {
+				$this->modx->queryTime += microtime(true) - $tstart;
+				$this->modx->executedQueries++;
+				while ($row = $q->stmt->fetch(PDO::FETCH_ASSOC)) {
+					$groups[$row['document_group']] = $row['profit'];
+				}
+			}
+			$this->MlmSystem->setCache($groups, $options);
+		}
+		return $groups;
+	}
+
 }
